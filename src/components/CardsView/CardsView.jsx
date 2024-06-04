86db8063da9d86233d16d3e5f924ceb49bca55fd
Maxim E <maermolenko@mail.ru>
2024-06-04 22:04:13 +0300

    Formatting fixed

--- src/components/CardsView/CardsView.jsx
+++ src/components/CardsView/CardsView.jsx
@@ -2,18 +2,13 @@ import { ShopCard } from "./ShopCard"
 
 export function CardsView({ products }) {
     return (
-        <>
 
     <div className="cards-holder">
       { products.map((product, index) => {
         return (
-
             <ShopCard key = {index} product = { product }/>
-        )
-        
+        )    
       })}
     </div>
-
-        </>
     )
 }
